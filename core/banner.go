331c00b9aa55ce9b216a8ed4e736769da1161957
Kibyanski <82338746+Kibyanski@users.noreply.github.com>
2021-04-16 12:08:15 +0200

    Update banner.go

--- core/banner.go
+++ core/banner.go
@@ -1,3 +1,80 @@
+package core
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+const (
+	VERSION = "2.4.2"
+)
+
+func putAsciiArt(s string) {
+	for _, c := range s {
+		d := string(c)
+		switch string(c) {
+		case " ":
+			color.Set(color.BgRed)
+			d = " "
+		case "@":
+			color.Set(color.BgBlack)
+			d = " "
+		case "#":
+			color.Set(color.BgHiRed)
+			d = " "
+		case "W":
+			color.Set(color.BgWhite)
+			d = " "
+		case "_":
+			color.Unset()
+			d = " "
+		case "\n":
+			color.Unset()
+		}
+		fmt.Print(d)
+	}
+	color.Unset()
+}
+
+func printLogo(s string) {
+	for _, c := range s {
+		d := string(c)
+		switch string(c) {
+		case "_":
+			color.Set(color.FgWhite)
+		case "\n":
+			color.Unset()
+		default:
+			color.Set(color.FgHiBlack)
+		}
+		fmt.Print(d)
+	}
+	color.Unset()
+}
+
+func printUpdateName() {
+	nameClr := color.New(color.FgHiRed)
+	txt := nameClr.Sprintf("                 - --  Gone Phishing  -- -")
+	fmt.Fprintf(color.Output, "%s", txt)
+}
+
+func printOneliner1() {
+	handleClr := color.New(color.FgHiBlue)
+	versionClr := color.New(color.FgGreen)
+	textClr := color.New(color.FgHiBlack)
+	spc := strings.Repeat(" ", 10-len(VERSION))
+	txt := textClr.Sprintf("      by Kuba Gretzky (") + handleClr.Sprintf("@mrgretzky") + textClr.Sprintf(")") + spc + textClr.Sprintf("version ") + versionClr.Sprintf("%s", VERSION)
+	fmt.Fprintf(color.Output, "%s", txt)
+}
+
+func printOneliner2() {
+	textClr := color.New(color.FgHiBlack)
+	red := color.New(color.FgRed)
+	white := color.New(color.FgWhite)
+	txt := textClr.Sprintf("                   no ") + red.Sprintf("nginx") + white.Sprintf(" - ") + textClr.Sprintf("pure ") + red.Sprintf("evil")
+	fmt.Fprintf(color.Output, "%s", txt)
 }
 
 func Banner() {
@@ -23,7 +100,11 @@ func Banner() {
 	printLogo(`  \____/ \___|_|  |  __/|_|   \__ _|_| |_|\___|_|`)
 	fmt.Println()
 	putAsciiArt("      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@      \n")
+	printLogo(`                  | |                            `)
+	fmt.Println()
 	putAsciiArt("     @@@@@WW@@@WW@@WWW@@WW@@@WW@@@@@     ")
+	printLogo(`                  |_|                            `)
+	fmt.Println()
 	printUpdateName()
 	fmt.Println()
 	putAsciiArt("    @@@@@@WW@@@WW@@WWW@@WW@@@WW@@@@@@    \n")
